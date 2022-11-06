3d44918ccd55ea745052b8313943fa2fed967021
Rajashekar Vemula <vemula.rajashekar02@gmail.com>
2022-11-06 13:13:52 +0530

    added valid First Name program

--- Program.cs
+++ Program.cs
@@ -9,26 +9,6 @@
             ValidFirstName a = new ValidFirstName();
             bool val = a.FirstName(firstName);
 
-            Console.WriteLine("Enter Last Name");
-            string lastName = Console.ReadLine();
-            ValidLastName b = new ValidLastName();
-            bool val1 = b.LastName(lastName);
-
-            Console.WriteLine("Enter Email Id");
-            string emailId = Console.ReadLine();
-            ValidEmail c = new ValidEmail();
-            bool val2 = c.EmailId(emailId);
-
-            Console.WriteLine("Enter Mobile Number");
-            string mobileNumber = Console.ReadLine();
-            ValidMobileNumber d = new ValidMobileNumber();
-            bool val3 = d.MobileNumber(mobileNumber);
-
-            Console.WriteLine("Enter Password");
-            string password = Console.ReadLine();
-            ValidPassword e = new ValidPassword();
-            bool val4 = e.Password(password);
-
             if (val)
             {
                 Console.WriteLine("Valid FirstName");
@@ -36,43 +16,7 @@
             else
             {
                 Console.WriteLine("Invalid FirstName");
-            }
-
-            if (val1)
-            {
-                Console.WriteLine("Valid LastName");
-            }
-            else
-            {
-                Console.WriteLine("Invalid LastName");
-            }
-
-            if (val2)
-            {
-                Console.WriteLine("Valid Email");
-            }
-            else
-            {
-                Console.WriteLine("Invalid Email");
-            }
-
-            if (val3)
-            {
-                Console.WriteLine("Valid MobileNumber");
-            }
-            else
-            {
-                Console.WriteLine("Invalid MobilNumber");
-            }
-
-            if (val4)
-            {
-                Console.WriteLine("Valid Password");
-            }
-            else
-            {
-                Console.WriteLine("Invalid Passward");
-            }
+            }          
         }
     }
 }
---
+++ ValidFirstName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationRegex
+{
+    public class ValidFirstName
+    {
+        public static string User_FirstName = "^[A-Z]{1}[a-z]{2,}$";
+        public bool FirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, User_FirstName);
+        }
+    }
+}
