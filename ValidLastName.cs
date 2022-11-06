9c760c5c7682b7e9f20682355e7ddc8508cc7fce
Rajashekar Vemula <vemula.rajashekar02@gmail.com>
2022-11-06 13:15:47 +0530

    added valid Last Name program

--- ValidLastName.cs
+++ ValidLastName.cs
@@ -7,12 +7,18 @@ using System.Threading.Tasks;
 
 namespace UserRegistrationRegex
 {
-    public class ValidFirstName
+    public class ValidLastName
     {
         public static string User_FirstName = "^[A-Z]{1}[a-z]{2,}$";
         public bool FirstName(string firstName)
         {
             return Regex.IsMatch(firstName, User_FirstName);
+        }
+
+        public static string User_LastName = "^[A-Z]{1}[a-z]{2,}$";
+        public bool LastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, User_LastName);
         }
     }
 }
